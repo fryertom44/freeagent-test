2405a5a26a7b7bc46cfe97aeaa21d6d3c055c6ab
Tom Fryer <fryertom@hotmail.com>
2019-08-25 00:11:07 +0100

    Fix file path method

--- lib/currency_exchange.rb
+++ lib/currency_exchange.rb
@@ -8,19 +8,17 @@ module CurrencyExchange
   # Raises an exception if unable to calculate requested rate.
   # Raises an exception if there is no rate for the date provided.
   def self.rate(date, from_currency, to_currency)
-    ExchangeRateHistory.new(file_path).lookup(date, from_currency, to_currency)
+    ExchangeRateHistory.new(self.file_path).lookup(date, from_currency, to_currency)
   end
-
-    # run via command line
-  if __FILE__ == $0
-    run
-  end
-
-  private
 
   def self.file_path
     base_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
     data_dir = File.join(base_dir, "data")
     @@exchange_rate_file ||= File.join(data_dir, "eurofxref-hist-90d.json")
+  end
+
+    # run via command line
+  if __FILE__ == $0
+    run
   end
 end
