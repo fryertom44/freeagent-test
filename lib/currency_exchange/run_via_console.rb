60b979438f3744a9f69cc255f0f97057750e9f9c
Tom Fryer <fryertom@hotmail.com>
2019-08-26 10:46:22 +0100

    Add missing renamed file

---
+++ lib/currency_exchange/run_via_console.rb
@@ -0,0 +1,55 @@
+module CurrencyExchange
+  require 'optparse'
+  require 'optparse/date'
+
+  def self.run_via_console
+    begin
+    # default options:
+    options = {verbose: false}
+    
+    OptionParser.new do |opts|
+      # banner and separator are the usage description showed with '--help' or '-h'
+      opts.banner = "Usage: currency_exchange.rb [options]"
+      opts.separator "Returns the conversion rate from the given file using the parameters given"
+      opts.separator "Options:"
+      # options (switch - true/false)
+      opts.on("-v", "--verbose", "Verbose mode") do |v|
+        options[:verbose] = v
+      end
+      opts.on("-f", "--file FILE", "Path to file containing exchange rates (currently only supports JSON)") do |f|
+        options[:file] = f
+      end
+      opts.on("-d", "--date [DATE]", Date, "Date of exchange rate") do |d|
+        options[:date] = d
+      end
+      opts.on("--from CURRENCY", "From Currency (e.g. USD)") do |f|
+        options[:from] = f
+      end
+      opts.on("--to CURRENCY", "To Currency (e.g. GBP)") do |t|
+        options[:to] = t
+      end
+      opts.on("-h", "--help", "Prints help") do
+        puts opts
+        exit
+      end
+    end.parse!
+
+    @@exchange_rate_file = options[:file]
+    rate = CurrencyExchange.rate(*options.values_at(:date, :from, :to))
+
+    if options[:verbose]
+      puts <<~HEREDOC
+        ......................................................
+        Date: #{options[:date]}
+        Calculating rate from: #{options[:from]} to: #{options[:to]}
+        ......................................................
+      HEREDOC
+    end
+
+    puts "=> #{rate}"
+
+    rescue StandardError => e
+      print "#{e}\n"
+    end
+  end
+end
